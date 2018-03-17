b1c81738911c2374f83ca8b64e735cf9b31428ba
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-03-17 13:53:36 -0400

    Refactored

--- readAFile.js
+++ readAFile.js
@@ -1,19 +1,6 @@
-// Read a file
-// Write a program that prompts the user to enter a file name, and reads in the contents of the file, convert the text to all caps, and prints it out.
-
-// Assuming the file file1.txt contains the text: Hello, I am file 1.. Example output:
-
-// $ node cap_file.js
-// filename: file1.txt
-// HELLO, I AM FILE 1.
-// Trigger an error condition by running the program on a non-existent file. Your program should display the error message, and it should display something like:
-
-// $ node cap_file.js
-// filename: blah.txt
-// ENOENT: no such file or directory, open 'blah.txt'
-
-
-// READ A FILE
+// WRITE A PROGRAM THAT PROMPTS THE USER TO ENTER A FILE NAME AND READS IN THE CONTENTS 
+// OF THE FILE AND CONVERTS THE TEXT TO ALL CAPS AND PRINTS IT OUT.
+// ALSO TRIGGER AN ERROR CONDITION BY RUNNING THE PROGRAM ON A NON-EXISTENT FILE.
 
 var readline = require('readline');
 
@@ -24,11 +11,11 @@ var rl = readline.createInterface({
 
 rl.question('filename: ', (filenameAnswer) => {
     var fs = require('fs');
-    fs.readFile(filenameAnswer, function(err, fileContents) {
-        if (err) {
-            console.log(err.toString());
+    fs.readFile(filenameAnswer, function(error, fileContents) {
+        if (error) {
+            console.error(error.toString());
         } else {
-            console.log(fileContents.toString());
+            console.log(fileContents.toString().toUpperCase());
         }
     });
     rl.close();

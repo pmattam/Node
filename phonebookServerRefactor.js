7494186b17efa2c34affe9e7b26a3d9d42b621de
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-03-20 22:52:21 -0400

    Refactor

--- phonebookServerRefactor.js
+++ phonebookServerRefactor.js
@@ -58,21 +58,31 @@ let notFound = function(request, response) {
     response.end('404, Nothing Here!');
 };
 
-let routes = [
-    { method: 'GET', path: '/contacts/', handler: getContact },
-    { method: 'DELETE', path: '/contacts/', handler: deleteContact },
-    { method: 'PUT', path: '/contacts/', handler: updateContact },
-    { method: 'GET', path: '/contacts', handler: getContacts },
-    { method: 'POST', path: '/contacts', handler: postContacts }
-];
-
+// let routes = [
+//     { method: 'GET', path: '/contacts/', handler: getContact },
+//     { method: 'DELETE', path: '/contacts/', handler: deleteContact },
+//     { method: 'PUT', path: '/contacts/', handler: updateContact },
+//     { method: 'GET', path: '/contacts', handler: getContacts },
+//     { method: 'POST', path: '/contacts', handler: postContacts }
+// ];
+var getHandler = function(method, path) {
+    let handler = (method === 'POST' && path === '/contacts') ? postContacts : (method === 'PUT') ? updateContact :
+        (method === 'DELETE') ? deleteContact : (method === 'GET' && path === '/contacts') ?
+        getContacts : (method === 'GET' && path === '/contacts/') ? getContact : notFound;
+    return handler;
+};
 let server = http.createServer(function(request, response) {
-    let route = routes.find(route => matches(request, route.method, route.path));
-    if (route) {
-        route.handler(request, response);
-    } else {
-        notFound(request, response);
-    }
+    // let route = routes.find(route => matches(request, route.method, route.path));
+    // // if (route) {
+    // //     route.handler(request, response);
+    // // } else {
+    // //     notFound(request, response);
+    // // }
+
+    // // route ? route.handler(request, response) : notFound(request, response);
+
+    // (route ? route.handler : notFound)(request, response);
+    getHandler(request.method, request.url)(request, response);
 });
 
 server.listen(3000);
